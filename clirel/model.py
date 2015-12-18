1b8e0046f1c56498908fd00ded46e04055956d97
c-cooper <cotiger3@gmail.com>
2015-12-18 13:07:06 -0500

    added prediction interface

--- clirel/model.py
+++ clirel/model.py
@@ -1,5 +1,5 @@
 # CliRel: model.py
-#	model object for predicting relations
+#    model object for predicting relations
 #
 # Connor Cooper
 
@@ -8,33 +8,39 @@ from features.features import getFeatureDict
 
 class Model:
 
-	def train(self, notes):
-		'''
-		Model:train()
-			train a model for predicting relations between concepts based on given training data
+    def train(self, notes):
+        '''
+        Model:train()
+            train a model for predicting relations between concepts based on given training data
 
-		@param notes: a list of notes with relation annotation data to train the model with
-		'''
+        @param notes: a list of notes with relation annotation data to train the model with
+        '''
 
-		featVectors = []
-		labels = []
+        featVectors = []
+        labels = []
 
-		# extract features and get labels for each note
-		for note in notes:
-			featVectors = featVectors + getFeatureDict(note)
-			labels = labels + note.getRelationLabels()
+        # extract features and get labels for each note
+        for note in notes:
+            featVectors = featVectors + getFeatureDict(note)
+            labels = labels + note.getRelationLabels()
 
-		# train classifier and vectorizer
-		clf, vec = sci.train(featVectors, labels)
+        # train classifier and vectorizer
+        clf, vec = sci.train(featVectors, labels)
 
-		# memoize classifier and vectorizer
-		self.clf = clf
-		self.vec = vec
+        # memoize classifier and vectorizer
+        self.clf = clf
+        self.vec = vec
 
-	def predict(self, notes):
-		# TODO: add prediction
-		pass
+    def predict(self, note):
 
+        # extract features
+        featDicts = getFeatureDict(note)
+
+        featVecs = vec.transform(featDicts)
+        labels = clf.predict(featVecs)
+
+        return labels
 
 if __name__ == "__main__":
-	print "nothing to do"
+    print "nothing to do"
+
