14c67f5d6cb4fa2d7c39da0f895fb390fe5d5d76
RenanC <renan_campos@student.uml.edu>
2017-01-08 22:32:06 -0500

    Cleaned evaluation code; performed sanity checks.

--- src/clirel.py
+++ src/clirel.py
@@ -1,4 +1,4 @@
-""" 
+"""
  Text-Machine Lab: CliRel
 
  File Name : clirel.py
@@ -14,6 +14,8 @@
 import os
 import sys
 import sklearn.metrics as metrics
+import numpy as np
+from collections import defaultdict
 
 import note
 
@@ -39,7 +41,7 @@ def train(t_dir, model_path, model_flags=None):
 
   # import the model.
   # The model will be loaded as a module. This provides modularity for the
-  # assumption that the model is implemented with a 'model.py' in its main dir. 
+  # assumption that the model is implemented with a 'model.py' in its main dir.
   sys.path.insert(0, absPath(model_path))
   import model
   sys.path = sys.path[1:]
@@ -51,7 +53,7 @@ def train(t_dir, model_path, model_flags=None):
 
 
 def predict(t_dir, model_path, model_flags=None):
-  
+
   cons = note.filterFiles(os.path.join(t_dir, 'concept'), 'con')
   txts = note.filterFiles(os.path.join(t_dir,     'txt'), 'txt')
 
@@ -65,7 +67,7 @@ def predict(t_dir, model_path, model_flags=None):
 
   # import the model.
   # The model will be loaded as a module. This provides modularity for the
-  # assumption that the model is implemented with a 'model.py' in its main dir. 
+  # assumption that the model is implemented with a 'model.py' in its main dir.
   sys.path.insert(0, model_path)
   import model
   sys.path = sys.path[1:]
@@ -76,41 +78,58 @@ def predict(t_dir, model_path, model_flags=None):
 
   return out
 
-def evaluate(g_dir, p_dir):
+def evaluate(e_dir, p_dir):
   """
-    Extracts the relation files from the gold directory and the 
+    Extracts the relation files from the gold directory and the
     prediciton directory, and calculates the F1, recall, and precision.
-    For this current iteration of the task, we are only concerned if the model
-    is able to identify a relation between two concepts (ignoring order).
   """
-  gold = dict()
-  pred = dict()
+
+  c_files = note.filterFiles(os.path.join(e_dir, 'concept'), 'con')
+  t_files = note.filterFiles(os.path.join(e_dir,     'txt'), 'txt')
+  g_files = note.filterFiles(os.path.join(e_dir,     'rel'), 'rel')
+  p_files = note.filterFiles(p_dir, 'pred')
+
+  def ugh():
+    return np.nan
+
+  gold = defaultdict(ugh)
+  pred = defaultdict(ugh)
 
   def addGold(d):
     ind = [int(d.conStart1), int(d.conEnd1), int(d.conStart2), int(d.conEnd2)]
-    # Ignore ordering
-    ind.sort()
     ind.append(d.lineNum)
     ind.append(d.fileName)
+
+    ind.append(d.conType1)
+    ind.append(d.conType2)
     gold[tuple(ind)] = d.relType
 
-  for g in note.filterFiles(g_dir, 'rel'):
-    rel = note.extractRels(g) 
-    if type(rel) == type(None):
-      continue
-    rel.apply(addGold, axis=1) 
-  
   def addPred(d):
     ind = [int(d.conStart1), int(d.conEnd1), int(d.conStart2), int(d.conEnd2)]
-    # Ignore ordering
-    ind.sort()
     ind.append(d.lineNum)
     ind.append(d.fileName)
+
+    ind.append(d.conType1)
+    ind.append(d.conType2)
+
     pred[tuple(ind)] = d.relType
 
-  for p in note.filterFiles(p_dir, 'pred'):
-    rel = note.extractRels(p) 
-    rel.apply(addPred, axis=1) 
+
+  for c, t, g in zip(c_files, t_files, g_files):
+    #print c.split('/')[-1].split('.')[0], t.split('/')[-1].split('.')[0], g.split('/')[-1].split('.')[0]
+    assert c.split('/')[-1].split('.')[0] == t.split('/')[-1].split('.')[0] == g.split('/')[-1].split('.')[0]
+    data = note.createTraining(c, t, g)
+    if type(data) == type(None):
+      continue
+    data.apply(addGold, axis=1)
+
+  for c, t, p in zip(c_files, t_files, p_files):
+    #print c.split('/')[-1].split('.')[0], t.split('/')[-1].split('.')[0], p.split('/')[-1].split('.')[0]
+    assert c.split('/')[-1].split('.')[0] == t.split('/')[-1].split('.')[0] == p.split('/')[-1].split('.')[0]
+    data = note.createTraining(c, t, p)
+    if type(data) == type(None):
+      continue
+    data.apply(addPred, axis=1)
 
   TP = 0
   FP = 0
@@ -119,23 +138,24 @@ def evaluate(g_dir, p_dir):
   g_labels = list()
   p_labels = list()
   for p in pred.keys():
-    try:
-      gold[p]
-    except KeyError:
-      # Ignore negative classification
-      if pred[p][0] != 'N':
-        FP += 1
-  
+    if type(gold[p]) != str:
+      gold[p] = 'N%s%s' % (p[-2][:2].upper(), p[-1][:2].upper())
+      FP += 1
+
   for g in gold.keys():
-    g_labels.append(gold[g])
-    try: 
-      pred[g]
-      TP += 1
-      p_labels.append(pred[g])
-    except KeyError:
+    if type(pred[g]) != str:
+      if gold[g][0] != "N":
+        p_labels.append('N%s%s' % (g[-2][:2].upper(), g[-1][:2].upper()))
+        g_labels.append(gold[g])
       FN += 1
-      p_labels.append('O')
-  
+    else:
+      TP += 1
+# This ignores concept pairs that did not have a relation.
+# Meaning this metric won't take into account Falsely labeled pairs.
+      if gold[g][0] != "N":
+        p_labels.append(pred[g])
+        g_labels.append(gold[g])
+
   if (TP + FP):
     P = float(TP) / (TP + FP)
   else:
@@ -149,6 +169,8 @@ def evaluate(g_dir, p_dir):
   else:
     F1 = 0
 
+  labels = list(set(p_labels + g_labels))
+  labels.sort()
   print "-" * 80
   print "Relation Detection Statistics:"
   print ""
@@ -161,27 +183,72 @@ def evaluate(g_dir, p_dir):
   print "-" * 80
   print "Classification Statistics:"
   print ""
-  print "\tF1:        %.4f" % metrics.f1_score(g_labels, 
-                                               p_labels, 
-                                               average='micro')
-  print "\tPrecision: %.4f" % metrics.precision_score(g_labels, 
-                                                      p_labels,
-                                                      average='micro')
-  print "\tRecall:    %.4f" % metrics.recall_score(g_labels, 
-                                                   p_labels,
-                                                   average='micro')
+  _LABELS = list(set(['PIP', 'TeCP', 'TeRP', 'TrAP',
+                      'TrCP', 'TrIP', 'TrNAP', 'TrWP']).union(p_labels))
+  _LABELS.sort()
+  print "\t F1:        %.4f" % metrics.f1_score(g_labels,
+                                               p_labels,
+                                               average='micro',
+                                               labels=_LABELS)
   print ""
-  print (list(set(g_labels))+['O'])
-  print metrics.confusion_matrix(g_labels, 
-                                 p_labels, 
-                                 labels=list(set(g_labels))+['O'])
+  for label in _LABELS:
+    print "%5s " % label,
+  print ""
+  confusion = metrics.confusion_matrix(g_labels,
+                                       p_labels,
+                                       )#labels=_LABELS)
+  for row in confusion:
+    for col in row:
+      print "%4d" % col, " ",
+    print
   print "-" * 80
   print ""
- 
+
 if __name__ == '__main__':
   # Uncomment to run tests
   #import doctest
   #doctest.testmod()
+
+  if (len(sys.argv) < 2):
+    print "USAGE: clirel.py train|test|eval data_dir [model|pred_dir] [options]"
+    sys.exit(1)
+
+  if (sys.argv[1] == "train"):
+    try:
+      t_dir = sys.argv[2]
+      model = sys.argv[3]
+      opts  = sys.argv[4:]
+    except:
+      print "Invalid arguemnts"
+      print "USAGE: clirel.py train data_dir model options"
+      sys.exit(1)
+    print "Training..."
+    train(t_dir, model, opts)
+  elif (sys.argv[1] == "test"):
+    try:
+      t_dir = sys.argv[2]
+      model = sys.argv[3]
+      opts  = sys.argv[4:]
+    except:
+      print "Invalid arguments"
+      print "USAGE: clirel.py test data_dir model options"
+      sys.exit(1)
+    print "Predicting..."
+    predict(t_dir, model, opts)
+  elif (sys.argv[1] == "eval"):
+    try:
+      e_dir = sys.argv[2]
+      pred  = sys.argv[3]
+    except:
+      print "Invalid arguments"
+      print "USAGE: clirel.py eval data_dir pred_dir"
+      sys.exit(1)
+    print "Evaluating..."
+    evaluate(e_dir, pred)
+  else:
+    print "USAGE: clirel.py train|test|eval data_dir [model|pred_dir] [options]"
+    sys.exit(1)
+
   # Uncomment to test evaluate
   #evaluate('i2b2_examples/rel/', 'predictions/')
   #print "train"
@@ -191,11 +258,14 @@ if __name__ == '__main__':
   #print "evaluate"
   #evaluate('i2b2_examples/rel/', 'predictions/')
   #print "train"
-  #train('i2b2_examples/', 'kim/', ['insert'])
+  #train('i2b2_examples/', 'blrandom/')
   #print "predict"
-  #predict('i2b2_examples/', 'kim/', ['insert'])
+  #predict('i2b2_examples/', 'blrandom/')
   #print "evaluate"
-  #evaluate('i2b2_examples/rel/', 'predictions/')
+  #evaluate('i2b2_examples/concept',
+  #         'i2b2_examples/txt',
+  #         'i2b2_examples/rel/',
+  #         'predictions/')
   #print "train"
   #train('i2b2_examples/', 'kim/', ['suffix'])
   #print "predict"
@@ -204,11 +274,17 @@ if __name__ == '__main__':
   #evaluate('i2b2_examples/rel/', 'predictions/')
   #print "train"
   #train('../data/train/', 'kim/', ['../data/train/parse'])
+  #train('../data/train/', 'blrandom/', ['../data/train/parse'])
   #print "predict"
+  #predict('../data/train/', 'blrandom/', ['../data/train/parse'])
   #predict('../data/train/', 'kim/', ['../data/train/parse'])
   #print "evaluate"
-  #evaluate('../data/train/rel/', 'predictions/')
-  print "predict"
-  predict('../data/test/', 'kim/', ['../data/test/parse'])
-  print "evaluate"
-  evaluate('../data/test/rel/', 'predictions/')
+  #evaluate('../data/train/concept',
+  #         '../data/train/txt',
+  #         '../data/train/rel', 'predictions/')
+  #print "predict"
+  #predict('../data/test/', 'kim/', ['../data/test/parse'])
+#  print "evaluate"
+#  evaluate('../data/test/concept',
+#           '../data/test/txt',
+#           '../data/test/rel', 'predictions/')
--- src/kim/model.py
+++ src/kim/model.py
@@ -1,4 +1,4 @@
-""" 
+"""
  Text-Machine Lab: CliRel
 
  File Name : model.py
@@ -9,14 +9,14 @@
 
  Purpose : Defines an svm with a custom kernel.
            Kernels defined below.
-           This module defines a composite kernel consisting of an entity 
+           This module defines a composite kernel consisting of an entity
            kernel and a convolution tree kernel.
            based on 'Extracting Clinical Relations in Electronic Health Records
            Using Enriched Parse Trees' by (Jisung Kim, et al.)
 
 """
 
- 
+
 import tree
 import bParser
 import numpy as np
@@ -37,7 +37,7 @@ import note
 # Labels svm should train.
 _LABELS = ['TrIP','TrWP','TrCP',
            'TrAP','TrNAP','TeRP',
-           'TeCP','PIP', 'NTrP', 
+           'TeCP','PIP', 'NTrP',
            'NTeP','NPP']
 
 
@@ -73,7 +73,7 @@ def genNeg(x, l=None):
                                      (l.conStart2 == x.conStart1) &
                                      (l.conEnd2   == x.conEnd1)   ].empty:
     return x.relType
-  
+
   # Special cases due to noisy training data
   if x.conType1 == 'test' and x.conType2 == 'test':
     return np.nan
@@ -83,7 +83,7 @@ def genNeg(x, l=None):
 
   if not pd.isnull(x.relType):
     return x.relType
-  
+
   if (x.conType1 == 'treatment' and x.conType2 == 'problem') or \
      (x.conType2 == 'treatment' and x.conType1 == 'problem'):
     return 'NTrP'
@@ -107,7 +107,7 @@ def filterNegLabeled(x):
   if x.relType in _LABELS[-3:] and x.conStart1 > x.conStart2:
     return np.nan
   return x.relType
-  
+
 
 #
 # Feature extraction helpers
@@ -132,8 +132,8 @@ def insert(x):
   if V:
     print "Inserting: %s, line: %s" % (x.fileName, x.lineNum)
   return tree.createString(tree.insert(
-           tree.createTree(x.parse), 
-           cStart1, 
+           tree.createTree(x.parse),
+           cStart1,
            cEnd1,
            cType1,
            cStart2,
@@ -158,8 +158,8 @@ def suffix(x):
   if V:
     print "Suffixing: %s, line: %s" % (x.fileName, x.lineNum)
   return tree.createString(tree.suffix(
-           tree.createTree(x.parse), 
-           cStart1, 
+           tree.createTree(x.parse),
+           cStart1,
            cEnd1,
            cType1,
            cStart2,
@@ -180,8 +180,8 @@ def spt(x):
   if V:
     print "Finding spt for: %s, line: %s" % (x.fileName, x.lineNum)
   return tree.createString(tree.spt(
-           tree.createTree(x.parse), 
-           cStart1, 
+           tree.createTree(x.parse),
+           cStart1,
            cEnd2))
 
 def entityFeature(x):
@@ -241,13 +241,13 @@ def train(data, flags):
       continue
     # Generate negative examples
     X['relType'] = X.apply(genNeg, axis=1, args=(X[X['relType'].notnull()],))
-   
+
     # Split number of negative examples in half by taking only the ordered ones.
-    X['relType'] = X.apply(filterNegLabeled, axis=1)
+    #X['relType'] = X.apply(filterNegLabeled, axis=1)
 
     # Filter invalid entries (i.e. test/treatment combinations)
     X = X[X['relType'].notnull()]
-    
+
     if X.empty:
       continue
 
@@ -265,10 +265,10 @@ def train(data, flags):
         return 'Invalid flag.'
     else:
       X['parse'] = X.apply(spt, axis=1)
-   
+
     # Create entity vectors
     X['vec'] = X.apply(entityFeature, axis=1)
-    
+
     # Write to svm files
     for label in _LABELS:
       f_name = os.path.join(absPath('./svm'), label + '.tmp')
@@ -291,11 +291,11 @@ def train(data, flags):
           return
 
         X.apply(svmTrain, axis=1)
-  
+
   # Train svms
   for label in _LABELS:
     f_name = os.path.join(absPath('./svm'), label + '.tmp')
-    subprocess.call([absPath('./svm-light-TK-1.2.1/svm_learn'), 
+    subprocess.call([absPath('./svm-light-TK-1.2.1/svm_learn'),
                      '-t', '5',
                      '-S', '1',
                      '-D', '1',
@@ -309,28 +309,34 @@ def train(data, flags):
                      f_name,
                      f_name.split('.tmp')[0] + '.svm',
                     ])
-      
-    #os.remove(f_name)
-  
+
+    os.remove(f_name)
+
   # Returns nothing on success
   return
 
 def predict(data, flags):
 
   parses = note.filterFiles(flags[0], 'parse')
-  
+
   for ((c,t),p) in zip(data, parses):
     X = note.createTesting(c, t)
     if type(X) == type(None) or X.empty:
-      continue
-    
+      f_name = os.path.basename(t).split(".")[0]
+      with open(os.path.join(absPath('../predictions'),
+                               f_name + '.pred'), 'w') as f:
+        continue
+
     # Generate negative examples
     X['relType'] = X.apply(genNeg, axis=1)
-    
+
     # Filter invalid entries (i.e. test/treatment combinations)
     X = X[X['relType'].notnull()]
     if X.empty:
-      continue
+      f_name = os.path.basename(t).split(".")[0]
+      with open(os.path.join(absPath('../predictions'),
+                               f_name + '.pred'), 'w') as f:
+        continue
 
     # Obtain parse trees
     P = bParser.extractPars(p)
@@ -346,10 +352,10 @@ def predict(data, flags):
         return 'Invalid flag.'
     else:
       X['parse'] = X.apply(spt, axis=1)
-    
+
     # Create entity vectors
     X['vec'] = X.apply(entityFeature, axis=1)
-    
+
 
     # Make predictions
     f_name = os.path.join(absPath("./svm"), 'predict.tmp')
@@ -373,8 +379,8 @@ def predict(data, flags):
     for label in _LABELS:
       m_name = os.path.join(absPath("./svm"), label + '.svm')
       r_name = os.path.join(absPath("./svm"), label + '.predict')
-      subprocess.call([absPath('./svm-light-TK-1.2.1/svm_classify'), 
-                       f_name, 
+      subprocess.call([absPath('./svm-light-TK-1.2.1/svm_classify'),
+                       f_name,
                        m_name,
                        r_name])
       r_label = list()
@@ -388,27 +394,28 @@ def predict(data, flags):
         results = np.concatenate((results,
                                   np.array(r_label).reshape(len(r_label),1)),
                                   axis=1)
-      os.remove(r_name)    
+      os.remove(r_name)
     os.remove(f_name)
-    
+
     out = [_LABELS[r] for r in results.argmax(axis=1)]
 
     X['relType'] = out
 
+    # Write predictions to file
+    f_name = set(X['fileName']).pop()
+
     # Filter out non-positive labels
     X['relType'] = X.apply(filterUnlabeled,axis=1)
     X = X[X['relType'].notnull()]
-    
-    if X.empty:
-      continue
 
-    # Write predictions to file
-    f_name = set(X['fileName']).pop()
-    with open(os.path.join(absPath('../predictions'), 
+    with open(os.path.join(absPath('../predictions'),
                              f_name + '.pred'), 'w') as f:
       def writeToFile(d):
         f.write(note.writeRel(d) + '\n')
-      
+
+      if X.empty:
+        continue
+
       X.apply(writeToFile, axis=1)
 
   return
